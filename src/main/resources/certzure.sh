b3d39cc1aad7b8e221119f05a7ebddab1970b271
Viktor Berke <github.bviktor@outlook.com>
2017-02-01 16:01:20 +0100

    Handle the case of unknown operations and support HPKPinx

--- src/main/resources/certzure.sh
+++ src/main/resources/certzure.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-java -jar /opt/certzure/certzure.jar $1 $2 $3 $4
+if [ $1 = 'deploy_challenge' ] || [ $1 = 'clean_challenge' ] || [ $1 = 'deploy_cert' ]
+then
+    java -jar /opt/certzure/certzure.jar $1 $2 $3 $4
+
+    if [ $1 = 'deploy_cert' ] && [ -e /opt/hpkpinx/hpkpinx.sh ]
+    then
+        sh /opt/hpkpinx/hpkpinx.sh $1 $2
+    fi
+fi
